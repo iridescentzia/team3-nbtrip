80f67cc22dc94daa8dae5f780ef3f2be8a11a15a
subeen <sju081902@gmail.com>
2025-07-22 16:53:30 +0900

    feat : 토큰 발급 및 검증 로직 추가

--- backend/src/main/java/org/scoula/security/accounting/service/CustomUserDetailsService.java
+++ backend/src/main/java/org/scoula/security/accounting/service/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package org.scoula.security.accounting.service;
 
-public class CustomUserDetailsService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.security.accounting.domain.CustomUser;
+import org.scoula.security.accounting.domain.MemberVO;
+import org.scoula.security.accounting.mapper.UserDetailsMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserDetailsMapper userDetailsMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberVO memberVO = userDetailsMapper.get(username);
+        return new CustomUser(memberVO);
+    }
 }
