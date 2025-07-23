d819cb4a04e6f5d98518a6f85183677241469b40
subeen <sju081902@gmail.com>
2025-07-23 17:59:36 +0900

    refactor : MyBatis Mapper @Param 어노테이션 추가

--- backend/src/main/java/org/scoula/security/accounting/mapper/UserDetailsMapper.java
+++ backend/src/main/java/org/scoula/security/accounting/mapper/UserDetailsMapper.java
@@ -1,19 +1,13 @@
 package org.scoula.security.accounting.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.scoula.security.accounting.domain.MemberVO;
 
 @Mapper
 public interface UserDetailsMapper {
-    // 사용자 조회(email - 로그인)
-    MemberVO getByEmail(String email);
-
-    // 사용자 조회(nickname)
-    MemberVO getByNickname(String nickname);
-
-    // 사용자 조회(userId)
-    MemberVO getById(int userId);
-
-    // FCM 토큰 업데이트
-    int updateFcmToken(String email, String fcmToken);
+    MemberVO getByEmail(String email);  // 사용자 조회(email - 로그인)
+    MemberVO getByNickname(String nickname);  // 사용자 조회(nickname)
+    MemberVO getById(int userId);  // 사용자 조회(userId)
+    void updateFcmToken(@Param("email") String email, @Param("fcmToken") String fcmToken);  // FCM 토큰 업데이트
 }
