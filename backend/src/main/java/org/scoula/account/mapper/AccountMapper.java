69f2e91552781700ef6de04efd3b1c2a4e37919d
yel0732 <yerilee1210@gmail.com>
2025-07-23 20:41:26 +0900

    feat: QR 결제 처리 로직 구현(Mapper, Service, Controller)
    - PaymentMapper 보완 및 XML 작성: 결제 내역 저장, 잔액 처리 로직 분리
    - AccountMapper 및 XML 작성: 사용자 잔액 차감, 사업자 잔액 증가 처리
    - PaymentService 구현: 트랜잭션 기반 QR 결제 로직 작성
    - PaymentController 작성: /api/transactions/qr POST 요청 처리

--- backend/src/main/java/org/scoula/account/mapper/AccountMapper.java
+++ backend/src/main/java/org/scoula/account/mapper/AccountMapper.java
@@ -1,4 +1,12 @@
 package org.scoula.account.mapper;
 
-public class AccountMapper {
+import org.apache.ibatis.annotations.Param;
+
+public interface AccountMapper {
+
+    // 사용자 계좌 잔액 차감
+    int decreaseUserBalance(@Param("userId") int userID, @Param("amount") int amount);
+
+    // 가맹점 사업자 계좌 잔액 증가
+    int increaseMerchantBalance(@Param("merchantId") int merchantID, @Param("amount") int amount);
 }
