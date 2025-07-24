1ff1219df5ead3876669ca28e56f438247b960de
yel0732 <yerilee1210@gmail.com>
2025-07-25 01:39:19 +0900

    feat: Account Service 및 Controller 구현
    - AccountService, AccountServiceImpl 작성
    - AccountController 작성

--- backend/src/main/java/org/scoula/account/service/AccountService.java
+++ backend/src/main/java/org/scoula/account/service/AccountService.java
@@ -1,4 +1,26 @@
 package org.scoula.account.service;
 
+import org.scoula.account.dto.AccountRegisterDTO;
+import org.scoula.account.dto.AccountUpdateDTO;
+import org.scoula.account.dto.AccountViewDTO;
+
 public interface AccountService {
+
+    // 계좌 등록
+    void registerAccount(AccountRegisterDTO accountRegisterDTO);
+
+    // 계좌 수정
+    void updateAccount(AccountUpdateDTO accountUpdateDTO);
+
+    // 계좌 조회
+    AccountViewDTO getAccountByUserId(int userId);
+
+    // 사용자 계좌 잔액 차감
+    boolean decreaseUserBalance(int userId, int amount);
+
+    // 사용자 계좌 잔액 증가
+    boolean increaseUserBalance(int userId, int amount);
+
+    // 사업자 계좌 잔액 증가
+    boolean increaseMerchantBalance(int merchantId, int amount);
 }
