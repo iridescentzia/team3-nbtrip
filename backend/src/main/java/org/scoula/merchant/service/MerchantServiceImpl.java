eeb15c5d6c939e3fa1ab1c1234496797e0538135
Wonyoung Kim <wonyoung020626@gmail.com>
2025-07-24 16:39:14 +0900

    feat : add MerchantService

--- backend/src/main/java/org/scoula/merchant/service/MerchantServiceImpl.java
+++ backend/src/main/java/org/scoula/merchant/service/MerchantServiceImpl.java
@@ -1,4 +1,39 @@
 package org.scoula.merchant.service;
 
-public class MerchantServiceImpl {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.merchant.domain.MerchantAccountVO;
+import org.scoula.merchant.domain.MerchantCategoryVO;
+import org.scoula.merchant.domain.MerchantVO;
+import org.scoula.merchant.dto.MerchantAccountDTO;
+import org.scoula.merchant.dto.MerchantCategoryDTO;
+import org.scoula.merchant.dto.MerchantDTO;
+import org.scoula.merchant.mapper.MerchantMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MerchantServiceImpl implements MerchantService {
+    final private MerchantMapper mapper;
+
+    @Override
+    public MerchantDTO getMerchant(int merchantId) {
+        MerchantVO merchant = mapper.getMerchant(merchantId);
+        return MerchantDTO.of(merchant);
+    }
+
+    @Override
+    public MerchantCategoryDTO getMerchantCategory(int categoryId) {
+        MerchantCategoryVO merchantCategory = mapper.getMerchantCategory(categoryId);
+        return MerchantCategoryDTO.of(merchantCategory);
+    }
+
+    @Override
+    public MerchantAccountDTO getMerchantAccount(int merchantId) {
+        MerchantAccountVO merchantAccount = mapper.getMerchantAccount(merchantId);
+        return MerchantAccountDTO.of(merchantAccount);
+    }
 }
