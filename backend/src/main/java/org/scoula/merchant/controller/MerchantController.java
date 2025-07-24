fc216fa790b467cee6880691313055cf48434217
Wonyoung Kim <wonyoung020626@gmail.com>
2025-07-24 16:39:44 +0900

    feat : add MerchantController

--- backend/src/main/java/org/scoula/merchant/controller/MerchantController.java
+++ backend/src/main/java/org/scoula/merchant/controller/MerchantController.java
@@ -1,4 +1,24 @@
 package org.scoula.merchant.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.merchant.dto.MerchantDTO;
+import org.scoula.merchant.service.MerchantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/merchants")
 public class MerchantController {
+    final MerchantService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MerchantDTO> getMerchant(@PathVariable("id") int id) {
+        return ResponseEntity.ok(service.getMerchant(id));
+    }
 }
