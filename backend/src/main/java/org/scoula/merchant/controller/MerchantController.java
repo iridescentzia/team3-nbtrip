2f557a13e5d71d8f439848d36371662610a4568c
Wonyoung Kim <wonyoung020626@gmail.com>
2025-07-24 22:21:36 +0900

    feat : add methods to get category and account

--- backend/src/main/java/org/scoula/merchant/controller/MerchantController.java
+++ backend/src/main/java/org/scoula/merchant/controller/MerchantController.java
@@ -2,6 +2,8 @@ package org.scoula.merchant.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.scoula.merchant.dto.MerchantAccountDTO;
+import org.scoula.merchant.dto.MerchantCategoryDTO;
 import org.scoula.merchant.dto.MerchantDTO;
 import org.scoula.merchant.service.MerchantService;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +22,15 @@ public class MerchantController {
     @GetMapping("/{id}")
     public ResponseEntity<MerchantDTO> getMerchant(@PathVariable("id") int id) {
         return ResponseEntity.ok(service.getMerchant(id));
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<MerchantCategoryDTO> getMerchantCategory(@PathVariable("id") int id) {
+        return ResponseEntity.ok(service.getMerchantCategory(id));
+    }
+
+    @GetMapping("/account/{id}")
+    public ResponseEntity<MerchantAccountDTO> getMerchantAccount(@PathVariable("id") int id) {
+        return ResponseEntity.ok(service.getMerchantAccount(id));
     }
 }
