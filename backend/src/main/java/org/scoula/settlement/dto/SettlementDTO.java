1fc71539acc70fdbc92cbb42c0dd21cbf59dfd9b
iridescentzia <ziaunzip@naver.com>
2025-07-24 14:45:12 +0900

    test:테스트때문에 추가했던 dto 삭제

--- backend/src/main/java/org/scoula/settlement/dto/SettlementDTO.java
+++ backend/src/main/java/org/scoula/settlement/dto/SettlementDTO.java
@@ -6,48 +6,4 @@ import java.util.List;
 
 public class SettlementDTO {
 
-    @Data
-    public static class SettlementSummaryResponseDto {
-        private String tripName;
-        private int totalAmount;
-        private List<MemberPaymentInfo> memberPayments;
-    }
-
-
-    @Data
-    public static class MemberPaymentInfo {
-        private String nickname;
-        private int amount;
-    }
-
-
-    @Data
-    public static class SettlementResultResponseDto {
-        private int totalAmount;
-        private List<OptimizedTransaction> transactions;
-    }
-
-
-    @Data
-    public static class OptimizedTransaction {
-        private String senderNickname;
-        private String receiverNickname;
-        private int amount;
-    }
-
-    @Data
-    public static class PersonalSettlementResponseDto {
-        private List<OptimizedTransaction> toReceive;
-        private List<OptimizedTransaction> toSend;
-    }
-
-    @Data
-    public static class CreateSettlementRequestDto {
-        private Long tripId;
-    }
-
-    @Data
-    public static class UpdateSettlementStatusRequestDto {
-        private String status; // "PROCESSING" 또는 "COMPLETED"}
-    }
 }
