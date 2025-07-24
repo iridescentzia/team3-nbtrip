a58489007c935e8bf2f964bc5fbd71a5f1b77148
seunguk99 <seunguk99@gmail.com>
2025-07-24 16:11:45 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성 feat : 백엔드 API 1차 완성 ################ # 본문(추가 설명)을 아랫줄에 작성 테스트 데이터로 작동하게 만들어서 Security 완성 후 합쳐야함 ################ # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- backend/src/main/java/org/scoula/group/service/GroupService.java
+++ backend/src/main/java/org/scoula/group/service/GroupService.java
@@ -1,11 +1,20 @@
 package org.scoula.group.service;
 
 
-import org.scoula.group.dto.GroupCreateDTO;
+import org.scoula.group.dto.GroupDTO;
+import org.scoula.group.dto.GroupMemberDTO;
 
 import java.util.List;
 
+
 public interface GroupService {
-    GroupCreateDTO get(int id);
-    List<GroupCreateDTO> getOnesGroups(int ownerId);
+    GroupDTO get(int groupId);
+    List<GroupMemberDTO> getGroupMembers(int groupId);
+    List<GroupDTO> getJoinedGroups(int userId);
+    GroupDTO inviteMember(int groupId, int userId);
+    int joinGroup(int groupId, int userId);
+    int changeMemberStatus(int groupId, int userId);
+    int changeGroupStatus(int groupId);
+    GroupDTO createGroup(GroupDTO groupDTO);
+    boolean isOwner(int groupId, int userId);
 }
