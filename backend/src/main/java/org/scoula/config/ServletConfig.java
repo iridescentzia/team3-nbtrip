d4d32d72326eb37ed4309836ab286688591631b9
iridescentzia <ziaunzip@naver.com>
2025-07-25 22:56:34 +0900

    refactor:백,프론트 연동을 위한 config 변경

--- backend/src/main/java/org/scoula/config/ServletConfig.java
+++ backend/src/main/java/org/scoula/config/ServletConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -17,7 +14,19 @@ public class ServletConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+
+        // frontend의 static 경로 파일 (css, js, html)
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("/resources/assets/");
+    }
+
+    // 선언적 코드(java, properties, xml, yaml 대체 가능)
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/resources/index.html");
     }
 
     @Bean
--- backend/src/main/java/org/scoula/config/WebConfig.java
+++ backend/src/main/java/org/scoula/config/WebConfig.java
@@ -32,15 +32,15 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
         return new String[] { "/" };
     }
 
-    @Override
-    protected Filter[] getServletFilters() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-
-        return new Filter[] { characterEncodingFilter };
-    }
+//    @Override
+//    protected Filter[] getServletFilters() {
+//        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+//
+//        characterEncodingFilter.setEncoding("UTF-8");
+//        characterEncodingFilter.setForceEncoding(true);
+//
+//        return new Filter[] { characterEncodingFilter };
+//    }
 
     @Override
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
