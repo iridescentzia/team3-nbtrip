658126ea16d2516a08939a6d99ccb509c287fda1
iridescentzia <ziaunzip@naver.com>
2025-07-25 17:44:10 +0900

    feat:mypage ui 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/home/Home.vue";
 import settlementRoutes from "@/router/settlementRoutes.js";
+import memberRoutes from "@/router/memberRoutes.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path: '/', name: 'home', component: Home},
       ...settlementRoutes,
+      ...memberRoutes,
   ],
 })
 
