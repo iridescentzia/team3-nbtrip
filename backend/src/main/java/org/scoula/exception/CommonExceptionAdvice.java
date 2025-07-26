bbe22b5426b649de19a939c0fde4b6a62b5a46e4
Wonyoung Kim <wonyoung020626@gmail.com>
2025-07-26 09:28:27 +0900

    feat : edit configurations

--- backend/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
+++ backend/src/main/java/org/scoula/exception/CommonExceptionAdvice.java
@@ -1,6 +1,7 @@
 package org.scoula.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,22 +13,28 @@ import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @ControllerAdvice
+@Order(1)
 public class CommonExceptionAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public String except(Exception ex, Model model) {
-        log.error("Exception......" + ex.getMessage());
-        model.addAttribute("exception", ex);
-
-        return "error_page";
-    }
-
     @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handle404(NoHandlerFoundException ex, HttpServletRequest request, Model model) {
-        log.error("404 예외 발생 -> ", ex);
-        model.addAttribute("uri", request.getRequestURI());
-
-        return "custom404";
+    public String handle404(NoHandlerFoundException ex) {
+        return "/resources/index.html";
     }
+
+//    @ExceptionHandler(Exception.class)
+//    public String except(Exception ex, Model model) {
+//        log.error("Exception......" + ex.getMessage());
+//        model.addAttribute("exception", ex);
+//
+//        return "error_page";
+//    }
+//
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    public String handle404(NoHandlerFoundException ex, HttpServletRequest request, Model model) {
+//        log.error("404 예외 발생 -> ", ex);
+//        model.addAttribute("uri", request.getRequestURI());
+//
+//        return "custom404";
+//    }
 }
