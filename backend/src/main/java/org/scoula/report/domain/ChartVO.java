9bfcd35fff7e3b41c9925332bb36969347f60bf6
Minho Lee <3570js@naver.com>
2025-07-26 09:19:08 +0900

    ChartController,ChartVO,ChartDTO,ChartMapper,ChartService,ChartServiceImpl 수정

--- backend/src/main/java/org/scoula/report/domain/ChartVO.java
+++ backend/src/main/java/org/scoula/report/domain/ChartVO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 /**
  * 차트에 사용할 도메인 VO.
  *  - 도넛 차트: category, totalAmount 사용
@@ -15,6 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ChartVO {
+
+    //payment 테이블
+    private Date payAt; //결제일시
+    private Integer amount; //결제금액
+    private Integer tripId; //여행 ID
+
+    //join으로 가져올 값
+    private String categoryName; //카테고리 종류
+    private String categoryId;   //카테고리 ID
+    private String merchantId;   //가맹점 ID
+    private String tripStatus;   //여행 상태
+
     /** 카테고리 이름 (도넛 차트) */
     private String category;
 
