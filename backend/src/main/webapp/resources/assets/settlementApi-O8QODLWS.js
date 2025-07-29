5a5b2e9238dda77a259823c30c05a730416570f9
Geonwoo Lee <tldnrjsdn123@naver.com>
2025-07-30 00:24:32 +0900

    feat: SettlementDetailView 추가
    + 개인화된 정산 내역 화면인SettlementDetailView.vue 파일 추가했습니다.
    
    + SettlementRoutes.js에 SettlementDetailView.vue화면 경로 설정 추가해뒀습니다.
    
    + SettlementApi.js에 개인화 정산 화면 관련 API 추가했습니다.
    
    + SettlementController에 테스트 용으로 userId 임시 데이터 처리 해놓았던거 지금은 주석 처리 해뒀는데 헷갈리시면 아예 지우셔도 됩니다!!

---
+++ backend/src/main/webapp/resources/assets/settlementApi-O8QODLWS.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-B9c21Z5E.js";const n=t=>e.get(`/settlements/${t}/summary`),a=t=>e.get(`/settlements/${t}/calculate`),r=t=>e.post("/settlements",t);export{a as c,n as g,r};
