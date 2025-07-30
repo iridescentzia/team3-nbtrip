a39200061b562d735e29366d62bd93942d622d53
Minho Lee <3570js@naver.com>
2025-07-30 11:40:27 +0900

    refactor Mapper및 Service 코드 수정

--- backend/src/main/java/org/scoula/report/service/ChartServiceImpl.java
+++ backend/src/main/java/org/scoula/report/service/ChartServiceImpl.java
@@ -3,8 +3,8 @@ package org.scoula.report.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.scoula.report.dto.ChartDTO;
+import org.scoula.report.dto.TripInfoDTO;
 import org.scoula.report.mapper.ChartMapper;
-import org.scoula.report.service.ChartService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +17,7 @@ public class ChartServiceImpl implements ChartService {
     private final ChartMapper mapper;
 
     @Override
-    public List<ChartDTO> getDonutChart(String tripId) {
+    public List<ChartDTO> getDonutChart(Integer tripId) {
         log.info("Fetching donut chart data for tripId={}", tripId);
         return mapper.selectDonutChart(tripId)
                 .stream()
@@ -26,7 +26,7 @@ public class ChartServiceImpl implements ChartService {
     }
 
     @Override
-    public List<ChartDTO> getLineChart(String tripId) {
+    public List<ChartDTO> getLineChart(Integer tripId) {
         log.info("Fetching line chart data for tripId={}", tripId);
         return mapper
                 .selectLineChart(tripId)
@@ -36,10 +36,8 @@ public class ChartServiceImpl implements ChartService {
     }
 
     @Override
-    public String getTripStatus(String tripId,String tripStatus) {
-        log.info("getTripStatus ▶ tripId={} tripStatus={}", tripId,tripStatus);
-        return mapper
-                .getTripStatus(tripId,tripStatus);
+    public TripInfoDTO getTripInfo(Integer tripId) {
+        return mapper.selectTripInfo(tripId);
     }
 
 }
