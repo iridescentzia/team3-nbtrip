82285d56483b3d2b22ca5f51d71b12f25b33027c
Minho Lee <3570js@naver.com>
2025-08-01 10:51:24 +0900

    refactor 띄어쓰기 저장

--- frontend/src/views/paymentlist/PaymentListInfo.vue
+++ frontend/src/views/paymentlist/PaymentListInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-     <!-- computed된 filteredPayments를 순회하며 카드 렌더링 -->
+    <!-- computed된 filteredPayments를 순회하며 카드 렌더링 -->
     <ExpenseCard
       v-for="(item, index) in filteredPayments"
       :key="index"
@@ -12,38 +12,38 @@
 </template>
 
 <script setup>
-import { ref,computed, onMounted } from 'vue';
-import { useRoute } from 'vue-router'; 
+import { ref, computed, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import ExpenseCard from '@/views/paymentlist/PaymentListInfoCard.vue';
 import paymentApi from '@/api/paymentlistApi';
 
 const props = defineProps({
   dateRange: {
     type: Object,
-    default: () => ({ start: '', end: '' })
-  }
-})
+    default: () => ({ start: '', end: '' }),
+  },
+});
 
 // URL에서 tripId 추출
 const route = useRoute();
-const tripId = Number(route.params.tripId); 
+const tripId = Number(route.params.tripId);
 const payments = ref([]); // 서버에서 받아온 전체 결제 내역
 
 const filteredPayments = computed(() => {
-  const { start, end } = props.dateRange
-  console.log('[PaymentListInfo.vue] 현재 dateRange:', start, end)
+  const { start, end } = props.dateRange;
+  console.log('[PaymentListInfo.vue] 현재 dateRange:', start, end);
 
   // 시작/종료 날짜가 모두 비어있으면 전체 반환
-  if (!start && !end) return payments.value
+  if (!start && !end) return payments.value;
 
-  return payments.value.filter(p => {
+  return payments.value.filter((p) => {
     // ISO 문자열에서 yyyy-MM-dd 부분만 비교
-    const payDate = p.payAt.slice(0, 10)
-    if (start && end)   return payDate >= start && payDate <= end
-    if (start)          return payDate >= start
-    if (end)            return payDate <= end
-  })
-})
+    const payDate = p.payAt.slice(0, 10);
+    if (start && end) return payDate >= start && payDate <= end;
+    if (start) return payDate >= start;
+    if (end) return payDate <= end;
+  });
+});
 
 function formatSub(payer, time) {
   const t = new Date(time);
