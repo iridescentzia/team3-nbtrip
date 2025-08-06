acd5e085510b3a94f97be2e4163c607ce2c1ba8f
yel0732 <yerilee1210@gmail.com>
2025-08-06 11:17:57 +0900

    feat: 결제참여자 조회 API 추가, refactor: Payment 결제 수정 기능 리팩토링

--- backend/src/main/java/org/scoula/payment/service/PaymentService.java
+++ backend/src/main/java/org/scoula/payment/service/PaymentService.java
@@ -1,6 +1,9 @@
 package org.scoula.payment.service;
 
+import org.scoula.payment.domain.ParticipantVO;
 import org.scoula.payment.dto.PaymentDTO;
+
+import java.util.List;
 
 public interface PaymentService {
     // QR 결제 프로세스
@@ -12,4 +15,7 @@ public interface PaymentService {
     // 결제 상세 수정
     void updateQrPayment(int paymentId, PaymentDTO dto, int userId);
     void updateManualPayment(int paymentId, PaymentDTO dto, int userId);
+
+    List<ParticipantVO> getParticipantsByPaymentId(int paymentId);
+
 }
