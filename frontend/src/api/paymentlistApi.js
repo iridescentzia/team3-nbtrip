97a2c624e963ade50df5e699427d5dd9baca95d8
leehyemie <hyexxxmi@gmail.com>
2025-08-08 14:09:50 +0900

    feat: 결제 참여자 필터링 수정

--- frontend/src/api/paymentlistApi.js
+++ frontend/src/api/paymentlistApi.js
@@ -1,11 +1,14 @@
 import api from '@/api'; // src/api/index.js의 export default
 
 export default {
+  // 전체 결제 내역 불러오기
   async getPaymentList(tripId) {
     const { data } = await api.get(`/paymentlist/${tripId}`);
     console.log("payment data: ", data);
     return data;
   },
+  
+  // 단건 결제 내역 불러오기
     async getPaymentListById(paymentId) {
     const { data } = await api.get(`/paymentlist/payment/${paymentId}`);
     console.log("payment data: ", data);
